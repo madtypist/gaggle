b811d0ca1d47eb2c0d3fbaef181295a5b26a0755
Jessie <mad_typist@yahoo.com>
2013-02-13 19:12:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gaggle::Application.config.secret_token = '364af1c8c2680d7b98af6d3e1817afc541b00b36de84c4db0537d2c69aa763c18eb61cbf6a912c58f877c4144dbc5a0f09dc4cb64699e6ad14f0a2d17d14b07d'
