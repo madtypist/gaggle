1218e37273fc76812980a395a61d977713e4cc72
Jessie <mad_typist@yahoo.com>
2013-02-25 19:46:36 -0500

    Added bootstrap library. Added Edit and Update logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,28 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def create
+    @movie = Movie.new(params[:movie])
+    @movie.save
+
+    flash.notice = "Movie successfully added"
+
+    redirect_to movie_path(@movie)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes(params[:movie])
+
+    redirect_to movie_path(@movie)
+  end
 end
