563093e0f07d11f8703629159f506e098754a5de
Jessie <mad_typist@yahoo.com>
2013-03-04 21:24:43 -0500

    Add Omniauth controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Gaggle::Application.routes.draw do
-  devise_for :users
+  devise_for :users,
+              controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
   resources :movies
   # The priority is based upon order of creation:
