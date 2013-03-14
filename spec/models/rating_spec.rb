cf3bf0c6267ac8bd6f9a68928ac6522011342b93
Jessie <mad_typist@yahoo.com>
2013-03-13 23:03:53 -0400

    Add tests to make sure user and movie ids can't be mass assigned

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -9,8 +9,27 @@ describe Rating do
 
   subject { @rating }
 
+  it { should be_valid }
+
   describe "when user_id is not present" do
     before { @rating.user_id = nil }
     it { should_not be_valid }
   end
+
+  describe "accessible attributes" do
+
+    it "should not allow access to user_id" do
+      expect do
+        Rating.new(user_id: user.id)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+
+    it "should not allow access to movie_id" do
+      expect do
+        Rating.new(movie_id: 1)
+      end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+    end
+
+  end
+
 end
