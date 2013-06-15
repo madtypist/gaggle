aa1f892e15e03621c34f40ba41ae31d81a7d790c
Jessie <mad_typist@yahoo.com>
2013-06-15 12:26:57 -0400

    Probably shouldn't commit secret token to a public repo. Oops. Moved it to an ENV variable that can be set in the application.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gaggle::Application.config.secret_token = '364af1c8c2680d7b98af6d3e1817afc541b00b36de84c4db0537d2c69aa763c18eb61cbf6a912c58f877c4144dbc5a0f09dc4cb64699e6ad14f0a2d17d14b07d'
+Gaggle::Application.config.secret_token = ENV['APP_SESSION_TOKEN']
