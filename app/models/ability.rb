1e654397c4e592a4cfb70c809fe09d8447d195e2
Jessie <mad_typist@yahoo.com>
2013-06-15 15:59:05 -0400

    Tell app what to do when it encounters a CanCan exception

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,7 @@ class Ability
     #
       user ||= User.new # guest user (not logged in)
       #if user.admin?
-      if user.username == "admin"
+      if user_signed_in?
         can :manage, :all
       else
         can :read, :all
