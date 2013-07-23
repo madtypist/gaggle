7633013e902b5befbfaa58e58bcc8753878377c5
Jessie <mad_typist@yahoo.com>
2013-07-23 19:58:40 -0400

    Add first bit of cancan goodness to app. Create basic role for users. Add db migrations for it.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,9 +5,11 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
       user ||= User.new # guest user (not logged in)
-      #if user.admin?
-      if user_signed_in?
+
+      if user.role == "admin"
         can :manage, :all
+      elsif user
+        can [:read, :create, :update], :all
       else
         can :read, :all
       end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130327132503) do
+ActiveRecord::Schema.define(:version => 20130723231714) do
 
   create_table "movies", :force => true do |t|
     t.string   "title"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20130327132503) do
     t.string   "provider"
     t.string   "uid"
     t.string   "username"
+    t.string   "role"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
